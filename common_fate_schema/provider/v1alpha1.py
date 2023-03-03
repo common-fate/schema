08bba696e96e9cf8abcf0d957658f83f6a4f6bbe
chrnorm <17420369+chrnorm@users.noreply.github.com>
2023-03-03 16:54:47 +0000

    fix broken json()

--- common_fate_schema/provider/v1alpha1.py
+++ common_fate_schema/provider/v1alpha1.py
@@ -105,7 +105,7 @@ class Schema(BaseModel):
                 }
             )
 
-        super().json(*args, **kwargs)
+        return super().json(*args, **kwargs)
 
     class Config:
         schema_extra = {
